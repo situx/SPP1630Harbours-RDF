925f1072a680c92b28ec58b6c2bd00a09e6374b1
situx <timo.homburg@gmx.de>
2023-05-09 01:01:13 +0200

    fix end

--- spptordf.py
+++ spptordf.py
@@ -162,7 +162,7 @@ with open('source/HarbourDataRepository_001_Kroeger_2018.csv', newline='', encod
             triples.add("<"+str(cururi)+"> <http://www.w3.org/2006/time#hasTime> <"+str(cururi)+"_foundation> .\n")
             triples.add("<"+str(cururi)+"_foundation> <http://www.w3.org/2000/01/rdf-schema#label> \"\"\""+str(row["Name_mod"]).replace("\"","'")+" Foundation\"\"\"@en. \n")
             triples.add("<"+str(cururi)+"_foundation> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2006/time#TemporalEntity> . \n")
-            triples.add("<"+str(cururi)+"_foundation> <http://www.w3.org/2006/time#hasBeginning> <"+str(cururi)+"_foundation_end> . \n")
+            triples.add("<"+str(cururi)+"_foundation> <http://www.w3.org/2006/time#hasEnd> <"+str(cururi)+"_foundation_end> . \n")
             triples.add("<"+str(cururi)+"_foundation_end> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2006/time#Instant> . \n")
             triples.add("<"+str(cururi)+"_foundation_end> <http://www.w3.org/2000/01/rdf-schema#label> \"\"\""+str(row["Name_mod"]).replace("\"","'")+" Foundation End\"\"\"@en. \n")
             triples.add("<"+str(cururi)+"_foundation_end> <http://www.w3.org/2006/time#inXSDgYear>  \"\"\""+str(row["Date_max"])+"\"\"\"^^<http://www.w3.org/2001/XMLSchema#gYear> .\n")
